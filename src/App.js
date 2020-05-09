759c15afdea47e221da1881a341a85721cefd428
MisaelSantos <misaelsan2017@outlook.com>
2020-05-09 18:16:57 -0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './styleApp.css';
 
-const App = () =>{
-  return (
+function App(){
+
+  return(
     <div className="App">
-      hello
+      <form action="">
+        <label htmlFor="" className="lb">Adicionar pensamento do dia</label>
+        <textarea name="" id="" cols="80" rows="5" className="ta"></textarea>
+        <button className="btn">Adicionar</button>
+      </form>
     </div>
-  );
+  )
+
 }
 
 export default App;
