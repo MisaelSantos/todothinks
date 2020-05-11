c8a18dd13fc8652a6ac6f03a82a246e907381c8e
MisaelSantos <misaelsan2017@outlook.com>
2020-05-11 10:43:01 -0300

    add hooks form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 
+import useForm from "./hooks/useForm";
 import './styleApp.css';
 
 function App(){
+  const [{ values, loading }, handleChange, handleSubmit] = useForm();
+
+  const enviarContato = () => {
+    // faça o que for preciso :)
+    console.log(values);
+  };
 
   return(
     <div className="App">
-      <form action="">
-        <label htmlFor="" className="lb">Adicionar pensamento do dia</label>
-        <textarea name="" id="" cols="80" rows="5" className="ta"></textarea>
-        <button className="btn">Adicionar</button>
+      <form onSubmit={handleSubmit(enviarContato)}>
+        <label className="lb">Adicionar pensamento do dia</label>
+        <textarea onChange={handleChange} name="name" id="" cols="30" rows="10"></textarea>
+        <button type="submit" className="btn">{loading ? "Enviando..." : "Enviar"}</button>
       </form>
     </div>
   )
