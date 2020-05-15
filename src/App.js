4a4090c44c8c12d1d61c6c72808c24ebe6d360bf
MisaelSantos <misaelsan2017@outlook.com>
2020-05-15 10:52:19 -0300

    add localstorage, add item, remove item

--- src/App.js
+++ src/App.js
@@ -7,39 +7,58 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.list = [];
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
 
     this.state = {
       value: [],
-      todos: []
+      todos: JSON.parse(localStorage.getItem('myValueInLocalStorage')) || []
     }
+
+    this.list = JSON.parse(localStorage.getItem('myValueInLocalStorage')) || []
+
   }
 
   handleChange(event) {
+    event.preventDefault();
     this.setState({
       value: [event.target.value]
     });
   }
 
   handleSubmit(event) {
+    event.preventDefault();
 
+    if (this.state.value !== '') {
+      this.list.push(this.state.value);
 
-    this.list.push(this.state.value);
+      localStorage.setItem('myValueInLocalStorage', JSON.stringify(this.list));
 
-    localStorage.setItem('myValueInLocalStorage', JSON.stringify(this.list));
+      const addItem = [...this.state.todos, this.state.value];
+      this.setState({
+        value: '',
+        todos: addItem
+      })
+    }
+
+  }
+
+  deleteItem(key) {
+    const del = this.state.todos.filter((item, index) => index !== key)
+
+    localStorage.setItem('myValueInLocalStorage', JSON.stringify(del));
+    this.list = del;
 
     this.setState({
-      todos: JSON.parse(localStorage.getItem('myValueInLocalStorage')) || []
+      todos: del
     })
-
-    event.preventDefault();
   }
 
   render() {
+
     return (
+
       <div className="App">
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.value} className="thought" onChange={this.handleChange} />
@@ -47,11 +66,12 @@ class App extends React.Component {
         </form>
 
         <ul>
-          {  
-            this.state.todos.map(item => {
+          {
+            this.state.todos.map((item, index) => {
               return (
-                <li key={item.id}>
+                <li key={index}>
                   {item}
+                  <button onClick={() => this.deleteItem(index)}>X</button>
                 </li>
               )
             })
